1ee9782a1c2a62b721098adb782edc5b893a9b2c
Barrelrolla <chetkara@gmail.com>
2016-12-07 14:39:05 +0200

    fixed button function and added random word

--- scripts/menu.js
+++ scripts/menu.js
@@ -3,19 +3,26 @@ var MenuState = {
         this.load.image("button", "assets/images/button.png");
     },
     create: function () {
+        var coord = 50;
         this.game.stage.backgroundColor = "#ffffff";
         this.game.add.text(0, 0, "Select category:");
-        var coord = 50;
         for (var cat in categories) {
-            this.game.add.button(0, coord, "button", this.gameStart(categories[cat].words[0].word));
-            this.game.add.text(0, coord, cat)
+            var length = categories[cat].words.length;
+            var rng = Math.floor((Math.random() * length));
+            console.log(rng);
+            var button = this.game.add.button(0, coord, "button");
+            button.variable = categories[cat].words[rng].word;
+            button.inputEnabled = true;
+            button.events.onInputDown.add(this.gameStart, this);
+            this.game.add.text(0, coord, cat);
             coord += 50;
         }
     },
     update: function () {
 
     },
-    gameStart: function (word) {
+    gameStart: function (button) {
+        var word = button.variable;
         game.state.start("GameState", true, false, word);
     }
 };
